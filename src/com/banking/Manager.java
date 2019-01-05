b66d8bc4c76da4831bf6d7f633d8e99b50bab983
Adam Pod <adam.podoxin@gmail.com>
2019-01-05 12:56:29 -0800

    Create Balance method

--- src/com/banking/Manager.java
+++ src/com/banking/Manager.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class Manager {
 	public ArrayList<Account> accounts = new ArrayList<Account>();
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Podoxin Banking System.");
-		
+
 		Manager manager = new Manager();
 		manager.Scan();
 	}
@@ -17,26 +17,38 @@ public class Manager {
 		Scanner scanner = new Scanner(System.in);
 		String[] inputs = scanner.nextLine().split("\\s+");
 
-		if(inputs[0].equals("create")) {
+		if (inputs[0].equals("create")) {
 			try {
 				accounts.add(new Account(inputs[1], inputs[2], inputs[3]));
 				System.out.println("Created account " + inputs[1] + " " + inputs[2] + ".");
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("Account could not be created. Please try again.");
+			}
+		} else if(inputs[0].equals("balance")) {
+			try {
+				System.out.println("The balance of account " + inputs[1] + " " + inputs[2] + " is $" + FindAccountByName(inputs[1], inputs[2]).GetBalance());
+			} catch(Exception e) {
+				System.out.println("Account does not exist.");
+			}
+		} else if (inputs[0].equals("deposit")) {
+			try {
+				float amount = Float.parseFloat(inputs[1]);
+				FindAccountByName(inputs[2], inputs[3]).Deposit(amount);
+			} catch (Exception e) {
+				System.out.println("Deposit failed. Please try again.");
 			}
 		}
 
 		Scan();
 	}
-	
+
 	public Account FindAccountByName(String firstName, String lastName) {
-		for(Account account : accounts) 
-		{
-			if(account.GetFirstName().equals(firstName) && account.GetLastName().equals(lastName)) {
+		for (Account account : accounts) {
+			if (account.GetFirstName().equals(firstName) && account.GetLastName().equals(lastName)) {
 				return account;
 			}
 		}
-		
+
 		return null;
 	}
 }
